c6a1177ccd58c3acbc062c3c07c19bf33910088a
Thang <thangtccy@gmail.com>
2025-07-01 21:16:18 +0700

    Update path of sfml and mysql

--- MusicStore/MusicStore.cpp
+++ MusicStore/MusicStore.cpp
@@ -1,11 +1,12 @@
 // MusicStore.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#define NOMINMAX
 #include <iostream>
 #include <mysql.h>
-#include <Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
     std::cout << "Hello World!\n";
+    return 0;
 }
